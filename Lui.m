29de2a5dd78e981caad59bf9a0ce7dd37e30be05
Daniel Bigham <daniel.bigham@gmail.com>
2017-11-22 16:15:54 -0500

    Disable the reload function unless doing active Lui development, since it takes a couple hundred milliseconds to create.

--- Lui.m
+++ Lui.m
@@ -17,28 +17,31 @@ Begin["`Private`"]
 $LuiDir = FileNameDrop[FindFile["Lui`Lui`"], -1];
 $LuiJavaPath = FileNameJoin[{$LuiDir, "bin"}];
 
-With[{package = "Lui`"},
-With[{dir = DirectoryName[DirectoryName[FindFile[package]]]},
-	Lui`Private`$ReloadFunction = ReloadLui;
-	WUtils`WUtils`TabsOrSpaces[package] = "Tabs";
-	If [!ValueQ[$reloadLui],
-		$reloadLui =
-			CreateReloadFunctionForDirectory[
-				DirectoryName[DirectoryName[FindFile[package]]]
-			];
-	];
-	Lui`$UnitTestDir = FileNameJoin[{DirectoryName[DirectoryName[FindFile[package]]], "Tests"}];
-	WUtils`WUtils`NotebookTypeToDirectory[package] = FileNameJoin[{dir, "Notebooks"}];
-];
-];
-
-(* Reloads .m files in this directory if they've changed. *)
-ReloadLui[] := $reloadLui[]
-If [!ListQ[Global`$ReloadFunctions], Global`$ReloadFunctions = {}];
-Global`$ReloadFunctions =
-	DeleteDuplicates[
-		Append[Global`$ReloadFunctions, ReloadLui]
-	]
+(* Disabled unless doing active Lui development. *)
+If [False,
+    With[{package = "Lui`"},
+    With[{dir = DirectoryName[DirectoryName[FindFile[package]]]},
+        Lui`Private`$ReloadFunction = ReloadLui;
+        WUtils`WUtils`TabsOrSpaces[package] = "Tabs";
+        If [!ValueQ[$reloadLui],
+            $reloadLui =
+                CreateReloadFunctionForDirectory[
+                    DirectoryName[DirectoryName[FindFile[package]]]
+                ];
+        ];
+        Lui`$UnitTestDir = FileNameJoin[{DirectoryName[DirectoryName[FindFile[package]]], "Tests"}];
+        WUtils`WUtils`NotebookTypeToDirectory[package] = FileNameJoin[{dir, "Notebooks"}];
+    ];
+    ];
+    
+    (* Reloads .m files in this directory if they've changed. *)
+    ReloadLui[] := $reloadLui[];
+    If [!ListQ[Global`$ReloadFunctions], Global`$ReloadFunctions = {}];
+    Global`$ReloadFunctions =
+        DeleteDuplicates[
+            Append[Global`$ReloadFunctions, ReloadLui]
+        ]
+]
 
 End[]
 
