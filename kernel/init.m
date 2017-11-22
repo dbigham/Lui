570e912f2edfc1a59f70c08684c38301b6ab2ca0
Daniel Bigham <daniel.bigham@gmail.com>
2017-11-22 16:23:37 -0500

    Support a CustomReload function in Get["Lui`"]
    Also: Warn if $Conetxt has gotten messed up.

--- kernel/init.m
+++ kernel/init.m
@@ -1,3 +1,8 @@
+If [$Context =!= "Global`",
+    Print["WARNING: Lui's init.m seems to have been run when $Context is not Global`."];
+    Print["Rather, it is: ", $Context];
+];
+
 Get["Lui`Lui`"];
 
 WUtils`WUtils`ReloadFiles[];
@@ -6,6 +11,11 @@ Needs["Lui`Util`"];
 Needs["Lui`Actions`"];
 Needs["Lui`Parse`"];
 Needs["Lui`UI`"];
+
+(* Allow external systems to setup some code that gets run each time we do a Get["Lui`"]. *)
+If [DownValues[Lui`CustomReload] =!= {},
+    Lui`CustomReload[];
+];
 
 InitializeParser[];
 
